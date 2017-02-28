a27c1aa0187853df31145c18103d2672f32affb4
SABOYA Christopher <sc511465@s202p08m.iutnice.unice.fr>
2017-02-28 09:26:00 +0100

    Initial Commit

---
+++ Calculatrice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculatrice
+//
+//  Created by SABOYA Christopher on 28/02/2017.
+//  Copyright © 2017 SABOYA Christopher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
